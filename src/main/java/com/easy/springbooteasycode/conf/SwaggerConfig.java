24419d6966d4da8cba45c0d4a15890f8f69863aa
macia1 <77227168+macia1@users.noreply.github.com>
2022-08-27 14:14:29 +0800

    less

--- src/main/java/com/easy/springbooteasycode/conf/SwaggerConfig.java
+++ src/main/java/com/easy/springbooteasycode/conf/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * @author zengguosheng
+ * @author zenggs
  * @Date 2022/3/10 15:02
  */
 @Configuration
